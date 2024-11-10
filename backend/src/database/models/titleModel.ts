c547032e049fa167d6a637019d3524981f5a9e12
Pablo Miguel Andrade Santos <pablo.santos0829@gmail.com>
2024-11-10 20:41:35 -0300

    Update titleModel.ts

--- backend/src/database/models/titleModel.ts
+++ backend/src/database/models/titleModel.ts
@@ -1,3 +1,25 @@
 export default class titleModel {
+<<<<<<< Updated upstream
     
+=======
+    private client: Promise<PoolClient>;
+
+    constructor() {
+        this.client = connect();
+    }
+    
+    async addTitle(description: string) {
+        try {
+            const query: string = "INSERT INTO title (tx_description) VALUES ($1) RETURNING *";
+            const result: QueryResult<any> = await (await this.client).query(query, [description]);
+            return result.rows[0];
+        }catch (error) {
+            console.log(error);
+            throw error;
+        }finally {
+            (await this.client).release();
+        }
+        
+    }
+>>>>>>> Stashed changes
 }
