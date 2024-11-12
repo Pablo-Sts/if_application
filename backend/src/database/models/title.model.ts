a0b746fb639862caf4207f96939b8d9e93e9cebc
Pablo-Sts <pablo.santos0829@gmail.com>
2024-11-12 16:29:36 -0300

    Update title.model.ts
    To extends the model.ts class

--- backend/src/database/models/title.model.ts
+++ backend/src/database/models/title.model.ts
@@ -1,51 +1,36 @@
-import { PoolClient, QueryResult } from "pg";
-import connect from "../db";
+import { QueryResult } from "pg";
+import Model from "./model";
+import Title from "../../../classes/title";
 
-export default class titleModel {
-
-    private client: Promise<PoolClient>;
+export default class titleModel extends Model {
 
     constructor() {
-        this.client = connect();
-    }
-
-    async getTitle(id: number) {
-        try {
-            const query: string = "SELECT * FROM title WHERE id_title = $1";
-            const result: QueryResult<any> = await (await this.client).query(query);
-            return result.rows[0];
-        } catch (error) {
-            console.log(error);
-            throw error;
-        } finally {
-            (await this.client).release();
-        }
-    }
-
-    async getAllTitles() {
-        try {
-            const query: string = "SELECT * FROM title";
-            const result: QueryResult<any> = await (await this.client).query(query);
-            return result.rows[0];
-        }catch(error) {
-            console.log(error);
-            throw error;
-        } finally {
-            (await this.client).release();
-        }
+        super("title");
     }
     
-    async addTitle(description: string) {
+    async add(description: string) {
         try {
             const query: string = "INSERT INTO title (tx_description) VALUES ($1) RETURNING *";
             const result: QueryResult<any> = await (await this.client).query(query, [description]);
             return result.rows[0];
         }catch (error) {
-            console.log(error);
             throw error;
         }finally {
             (await this.client).release();
         }
         
     }
+
+    async update(title: Title) {
+        try {
+            const query: string = "UPDATE title SET tx_description = $2 WHERE id_title = $1 RETURNING *";
+            const result: QueryResult<any> = await (await this.client).query(query, [title.id, title.description]);
+            return result.rows[0];
+        } catch (error) {
+            throw error;
+        } finally {
+            (await this.client).release();
+        }
+    }
+
 }
